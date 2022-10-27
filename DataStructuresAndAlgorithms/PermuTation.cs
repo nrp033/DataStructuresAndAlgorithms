b2a9ff6b1e8e0e9b123f824bedfe8428d3704b69
Nitin Pawar <107263213+nrp033@users.noreply.github.com>
2022-10-27 08:55:57 +0530

    String Permutation Using Iterative and Recursive Method

---
+++ DataStructuresAndAlgorithms/PermuTation.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructuresAndAlgorithms
+{
+    internal class PermuTation
+    {
+        public static void permute(String str, int l, int r)
+        {
+            if (l == r)
+                Console.WriteLine(str);
+            else
+            {
+                for (int i = l; i <= r; i++)
+                {
+                   
+                    str = swap(str, l, i);
+                    permute(str, l + 1, r);
+                    str = swap(str, l, i);
+                }
+            }
+        }
+        public static String swap(String a,int i, int j)
+        {
+            char temp;
+            char[] charArray = a.ToCharArray();
+            temp = charArray[i];
+            charArray[i] = charArray[j];
+            charArray[j] = temp;
+            string s = new string(charArray);
+            return s;
+        }
+
+
+    }
+}
---
+++ DataStructuresAndAlgorithms/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace DataStructuresAndAlgorithms
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Write("\n\nEnter the String :  ");
+            String str = Console.ReadLine();
+            int n = str.Length;
+            PermuTation.permute(str, 0, n - 1);
+
+
+            Console.ReadLine();
+        }
+    }
+}
